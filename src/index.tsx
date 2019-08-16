d7f6d4b5a66316b5f28df5984710dc65fc17de99
WooLNinesun <woolninesun@gmail.com>
2019-08-16 17:04:30 +0800

    :checkered_flag: support IE9 ~ 11

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'normalize.css';
 import './index.scss';
 
 import GSXToWebApp from './GSXToWebApp';
